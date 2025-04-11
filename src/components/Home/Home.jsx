a90e1af0b9b5babadc5303b4a5feab89dde7a23b
Md Sahin Alam <sahinraj20@gmail.com>
2025-04-11 09:46:27 +0600

    hompage complete

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,11 +6,16 @@ function Home() {
       <div className="details">
         <div className="full-name">Md Sahin Alam</div>
         <div className="my-title">MERN Stack Developer</div>
-        <div className="article">I am a versatile MERN Stack Developer with a passion for both web development and competitive programming. My expertise lies in React, Tailwind CSS, and JavaScript, crafting seamless and user-friendly interfaces. On the backend, I am proficient in Node.js, Express.js, and MongoDB, ensuring scalable and efficient solutions.</div>
+        <div className="article">I design and develop scalable and high-performance digital solutions to meet the needs of modern businesses.</div>
+        <button className="glow-button mt-6">
+          Download CV
+          <span className="sparkle-circle"></span>
+        </button>
       </div>
       <div className="codingimage">
         <img src={codingImage} alt="Coding" />
       </div>
+
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import './Navbar.css'
-import { FaLinkedin } from "react-icons/fa";
+import { FaFacebook, FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
+import { FaXTwitter } from 'react-icons/fa6';
 
 function Navbar() {
     return (
         <div className='Navbar'>
-            <div className="name">a. rahman</div>
+            <div className="name">Md. Sahin Alam</div>
             <div className="contact">
-               <a href='https://www.linkedin.com/in/abdurrahman040/'><FaLinkedin className='linkedin' /></a> 
-               <a href='https://github.com/AbdurRahmanSikder'><FaGithub className='github' /></a>
+               <a href='https://www.linkedin.com/in/sahin404/'><FaLinkedin className='linkedin' /></a> 
+               <a href='https://github.com/sahin404'><FaGithub className='github' /></a>
+               <a href='https://www.facebook.com/sahin404'><FaFacebook className='github' /></a>
+               <a href='https://x.com/sahin404__'><FaXTwitter className='github' /></a>
             </div>
         </div>
     )
