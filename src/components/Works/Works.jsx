5c8efbef953fab0bcc8bdba88f80bd259fb92b8a
Md. Sahin Alan <sahinraj20@gmail.com>
2025-07-31 02:46:23 +0600

    website update

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -7,11 +7,11 @@ import tetris from '../../assets/tetris.png'
 import cart from '../../assets/cart.png'
 import { SiDart } from "react-icons/si";
 import { HiOutlineDocumentText } from "react-icons/hi";
-
+import message from '../../assets/message.png'
 const experience = [
     {
         id: 1,
-        title: "Technical Assistant",
+        title: "Lab Attendant",
         description: "EDGE-GSTUCSE Digital Skills Training",
         icon: (
             <img src={edge}
@@ -20,7 +20,7 @@ const experience = [
                 height="30"
             />
         ),
-        timeFrame: "Feb 2024 - Jan 2025"
+        timeFrame: "Feb 2024 - June 2025"
 
     }
 ];
@@ -28,6 +28,27 @@ const experience = [
 const projects = [
     {
         id: 1,
+        title: "DeliChat - Real-time Chatting Appilcation",
+        description: "A simple real-time chat app frontend built with React, Zustand for state management, and Socket.IO for live messaging.",
+        url: "https://mern-chat-app-2cls.onrender.com/", // Add link here
+        icon: (
+            <img src={message}
+                alt="messenger"
+                width="30"
+                height="30"
+            />
+        ),
+        technologies: [
+            { name: "MongoDB" },
+            { name: "Express" },
+            { name: "React" },
+            { name: "NodeJs" },
+            { name: "Socket.IO"},
+            { name: "Zustand"} 
+        ],
+    },
+    {
+        id: 2,
         title: "DeliBazar E-commerce Website",
         description: "A fast and user-friendly E-commerce platform built with the MERN stack.",
         url: "https://delibazar.onrender.com/", // Add link here
@@ -46,7 +67,7 @@ const projects = [
         ],
     },
     {
-        id: 2,
+        id: 3,
         title: "Tetris Game",
         description: "A simple Tetris game in Python using Pygame with smooth controls, collision detection",
         url: "https://github.com/sahin404/Python-Tetris-Game", // Add link here
